37f505a5596322f458dbc53f8447a99b33793d35
Newsworld <1715966050@qq.com>
2026-04-17 20:47:17 +0800

    2026.4.17

--- C++ Primer Plus/Chapter 10/10.5stock10.cpp
+++ C++ Primer Plus/Chapter 10/10.5stock10.cpp
@@ -1,11 +1,22 @@
-// stock00.cpp -- implementing the Stock class
-// version 00
+// stock10.cpp -- Stock class with constructors, destructor added
 #include <iostream>
-#include "10.1stock00.h"
+#include "10.4stock10.h"
 
-void Stock::acquire(const std::string & co, long n, double pr)
+// constructors (verbose versions)
+Stock::Stock()          // default constructor
 {
+    std::cout << "Default constructor called\n";
+    company = "no name";
+    shares = 0;
+    share_val = 0.0;
+    total_val = 0.0;
+}
+
+Stock::Stock(const std::string & co, long n, double pr)
+{
+    std::cout << "Constructor using " << co << " called\n";
     company = co;
+
     if (n < 0)
     {
         std::cout << "Number of shares can't be negative; "
@@ -17,7 +28,13 @@ void Stock::acquire(const std::string & co, long n, double pr)
     share_val = pr;
     set_tot();
 }
+// class destructor
+Stock::~Stock()         // verbose class destructor
+{
+    std::cout << "Bye, " << company << "!\n";
+}
 
+// other methods
 void Stock::buy(long num, double price)
 {
     if (num < 0)
@@ -65,15 +82,14 @@ void Stock::show()
     using std::ios_base;
     // set format to #.###
     ios_base::fmtflags orig = cout.setf(ios_base::fixed, ios_base::floatfield);
-    std::streamsize prec= cout.precision(3);
+    std::streamsize prec = cout.precision(3);
 
-    std::cout << "Company: " << company
-            << " Shares: " << shares << '\n'
-            << " Share Price: $" << share_val
-            << " Total Worth: $" << total_val << '\n';
+    cout << "Company: " << company
+        << " Shares: " << shares << '\n';
+    cout << " Share Price: $" << share_val;
     // set format to #.##
     cout.precision(2);
-    cout << "   Total Worth: $" << total_val << "\n";
+    cout << " Total Worth: $" << total_val << '\n';
 
     // restore original format
     cout.setf(orig, ios_base::floatfield);
